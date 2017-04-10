08132be7645053becac0cb1e54fbb20fd9594f96
MrowkaMINPG <slawomirautomatyczny@gmail.com>
2017-04-10 18:20:15 +0200

    gsdgsdg

--- scalanieciagow.cpp
+++ scalanieciagow.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
- 
+
 int main()
 {
 int tablica1[10] = { 1, 13, 43, 77, 88, 109, 121, 144, 165, 200 };
 int tablica2[10] = { 0, 23, 44, 77, 99, 101, 111, 150, 162, 199 };
 int tablica_wynikowa[20];
 int i, j, k; // zmienne pomocnicze
- 
+
 // wyswietlenie wygenerowanych ciagow
 cout << "Ciag pierwszy: ";
 for (i = 0; i < 10; i++)
 cout << tablica1[i] << " ";
 cout << endl;
-cout << "Ciag drugi: ";
+cout << "Ciag d1234214rugi: ";
 for (i = 0; i < 10; i++)
 cout << tablica2[i] << " ";
 cout << endl;
- 
+
 // scalanie ciagow
 for (i = j = k = 0; i < 10 && j < 10; k++)
 {
@@ -39,12 +39,12 @@ tablica_wynikowa[k] = tablica1[i];
 else
 for ( ; j < 10; j++, k++)
 tablica_wynikowa[k] = tablica2[j];
- 
+
 // wypisanie tablicy wynikowej
 cout << "Ciag posortowany: ";
 for (i = 0; i < 20; i++)
 cout << tablica_wynikowa[i] << " ";
 cout << endl;
- 
+
 return 0;
 }
