31567772669a5a6731733cbe496fea18785c0230
MrowkaMINPG <slawomirautomatyczny@gmail.com>
2017-04-10 18:04:47 +0200

    asfasfsadf

---
+++ xd/waznyprogram.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define S 6
+int main(void)
+{
+	printf("Ale bym co zjad");
+	double t;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	t=t++;
+	printf("\n nasze te po tym wszystkim %3d \n",t);
+	t=t++;
+	t=t++;
+	printf("wciśnij cokolwiek by zakończyć");
+	scanf(%d,&t);
+	t=t++;
+	return 0;
+}
