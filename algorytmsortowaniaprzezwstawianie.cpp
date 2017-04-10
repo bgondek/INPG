9a8d7186692ba19bb55a68e8b15639804b7ce823
MrowkaMINPG <slawomirautomatyczny@gmail.com>
2017-04-10 18:19:04 +0200

    f

--- algorytmsortowaniaprzezwstawianie.cpp
+++ algorytmsortowaniaprzezwstawianie.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
- 
+
 int main()
 {
 int ilosc_liczb, tymczasowa, i,j;
 cout << "Podaj ilosc licz do posortowania: ";
 cin >> ilosc_liczb;
 int *tablica = new int [ilosc_liczb]; // utworzenie dynamicznej tablicy na 'ilosc_liczb' elementow
- 
+
 for (i = 0; i < ilosc_liczb; i++)
 {
-cout << "Podaj liczba: ";
+cout << "Podaj liczbe: ";
 cin >> tablica[i];
 }
- 
+
 for (i = 1; i < ilosc_liczb; i++)
 {
 tymczasowa = tablica[i]; // przechowanie elementu tablicy z zmiennej pomocniczej
@@ -26,12 +26,12 @@ break;
 }
 tablica[j+1] = tymczasowa;
 }
- 
+
 for (i = 0; i < ilosc_liczb; i++) // wypisanie posortowanej tablicy
-cout << "tablica[" << i << "] = " << tablica[i] << endl;
- 
+cout << "tablgfsadheeica[" << i << "] = " << tablica[i] << endl;
+
 delete [] tablica; // zwolnienie tablicy zaalokowanej dynamicznie
- 
+
 return 0;
 }
 Dodaj komentarz
